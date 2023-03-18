dcc763e3b1e6e1b43f134d92f24f369987a486a3
root <liangguozhen2018@163.com>
2023-03-18 22:51:31 +0800

    Delete Checkpoints

---
+++ delete.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm -r ./Algorithm/BFS/.ipynb_checkpoints
+rm -r ./Algorithm/BackTrack/.ipynb_checkpoints
+rm -r ./Algorithm/BinarySearch/.ipynb_checkpoints
+rm -r ./Algorithm/BubbleSort/.ipynb_checkpoints
+rm -r ./Algorithm/CountingSort/.ipynb_checkpoints
+rm -r ./Algorithm/DFS/.ipynb_checkpoints
+rm -r ./Algorithm/DynamicProgramming/.ipynb_checkpoints
+rm -r ./Algorithm/HeapSort/.ipynb_checkpoints
+rm -r ./Algorithm/InsertionSort/.ipynb_checkpoints
+rm -r ./Algorithm/MergeSort/.ipynb_checkpoints
+rm -r ./Algorithm/QuickSort/.ipynb_checkpoints
+rm -r ./Algorithm/reverseLinkedList/.ipynb_checkpoints
+rm -r ./Algorithm/reverseString/.ipynb_checkpoints
+rm -r ./Algorithm/SelectionSort/.ipynb_checkpoints
+rm -r ./Algorithm/ShellSort/.ipynb_checkpoints
